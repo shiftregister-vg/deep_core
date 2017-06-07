bff1acc8329ea462bf1b04c24900fceb5fdbafc2
Steve Good <sgood@lanctr.com>
2017-06-07 15:38:08 -0700

    Updated HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,8 @@ class HomePage extends Component {
     return (
       <div className="home-page">
         <h1>Modern X-Wing Squad Builder</h1>
+        <h3>This is a work in progress</h3>
+        <p>As a way to better learn the available options for X-Wing Miniatures I decided to build another X-Wing squad builder.  Yes, I know there are plenty out there.  This is not an attempt to replace those.</p>
       </div>
     )
   }
