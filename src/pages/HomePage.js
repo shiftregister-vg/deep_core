8851dda98653dd81fa181b615421c1ab313d672f
Steve Good <sgood@lanctr.com>
2017-06-07 15:49:08 -0700

    Style baby.  Style.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './HomePage.css'
 
 class HomePage extends Component {
   render() {
@@ -8,7 +9,7 @@ class HomePage extends Component {
         <h1>Modern X-Wing Squad Builder</h1>
         <h3>This is a work in progress</h3>
         <p>As a way to better learn the available options for X-Wing Miniatures I decided to build another X-Wing squad builder.  Yes, I know there are plenty out there.  This is not an attempt to replace those.</p>
-        <p>While you're here, check out the <Link to="/pilot-sheet">Pilot Card sheet</Link> and <a href="https://github.com/stevegood/mxwsb/issues" target="_blank">let me know what I left out</a>.</p>
+        <p>While you're here, check out the <Link to="/pilot-sheet">Pilot Card sheet</Link> and <a href="https://github.com/stevegood/mxwsb/issues" target="_blank" rel="noopener noreferrer">let me know what I left out</a>.</p>
       </div>
     )
   }
