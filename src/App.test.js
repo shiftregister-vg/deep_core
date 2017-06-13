fe6e19700297f25a97835deef28c1dfe86cca844
Steve Good <sgood@lanctr.com>
2017-06-13 11:25:43 -0700

    Updated App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router } from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const browserHistory = createBrowserHistory()
+  ReactDOM.render(
+    <Router history={browserHistory}>
+      <App />
+    </Router>,
+    div
+  );
 });
