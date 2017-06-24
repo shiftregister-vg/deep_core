8ecc31a715e0db766919fea3d2c9861b96bfd045
Steve Good <sgood@lanctr.com>
2017-06-23 17:04:18 -0700

    started on crew data

--- src/data/upgrades/crew.js
+++ src/data/upgrades/crew.js
@@ -1,2 +1,11 @@
-const crew = []
+const crew = [{
+  id: 47,
+  name: 'Chewbacca',
+  type: 'crew',
+  unique: true,
+  restrictions: 'Rebel only.',
+  text: 'When you are dealt a Damage card, you may immediately discard that card and recover 1 shield.',
+  text2: 'Then, discard this upgrade card.',
+  value: 4
+}]
 export default crew.sort((a, b) => a.name > b.name ? 1 : a.name < b.name ? -1 : 0)
